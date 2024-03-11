5c021af6fd80a8a5bd8b31cf04162dc62f0c1785
Othmansalahi <ootmansalhi.24@gmail.com>
2024-03-11 23:17:01 +0000

    Add new files and remove unused files

---
+++ src/app/Profile/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Prfile = () => {
+  return (
+    <div>Prfile page</div>
+  )
+}
+
+export default Prfile
---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Notfound() {
+  return (
+    <div>Notfound page</div>
+  )
+}
+
+export default Notfound
